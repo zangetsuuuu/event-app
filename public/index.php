401eeecd49df2e5860c0750c5dec8985dcf78182
Rafif Athallah <rafifathallah99@gmail.com>
2024-02-16 21:53:38 +0700

    update: main.css, img, index.php

--- public/index.php
+++ public/index.php
@@ -2,18 +2,18 @@
 
 <main>
     <div class="px-4 pt-5 mt-2 text-center">
-        <h1 class="display-4 fw-bold">Welcome to Eventiqo!</h1>
-        <div class="col-lg-6 mx-auto">
+        <h1 class="display-4 fw-bold animate__animated animate__fadeIn animate__delay-1s">Welcome to Eventiqo!</h1>
+        <div class="col-lg-6 mx-auto animate__animated animate__fadeIn animate__delay-1s">
             <p class="lead mb-4">Our platform are offering unforgettable experiences in the world of events. Discover thousands of exciting events across various categories to suit your taste. Join now to gain exclusive access and don't miss out on special moments!</p>
             <div class="d-grid gap-1 d-sm-flex justify-content-sm-center mb-5">
-                <a href="pages/signup.php">
+                <a href="pages/register.php">
                     <button type="button" class="btn btn-dark btn-lg px-4 me-sm-3"><i class="fa-solid fa-location-arrow me-3"></i>Get Started!</button>
                 </a>
             </div>
         </div>
-        <div class="overflow-hidden" style="max-height: 50vh;">
+        <div class="overflow-hidden animate__animated animate__fadeInUp animate__delay-1s" style="max-height: 50vh;">
             <div class="container px-5">
-                <img src="public/img/event-img-1.jpg" class="img-fluid border rounded-3 shadow-lg mb-4"
+                <img src="public/img/webp/event-img-1.webp" class="img-fluid border rounded-3 shadow-lg mb-4"
                     alt="Example image" width="700" height="500" loading="lazy">
             </div>
         </div>
