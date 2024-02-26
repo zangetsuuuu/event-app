785bb4ff5ffa36b4b68b78dbe006031a8f3e5b6e
Rafif Athallah <rafifathallah99@gmail.com>
2024-02-26 23:41:18 +0700

    add: search popup

---
+++ includes/events.search.php
@@ -0,0 +1,23 @@
+<div class="modal fade" id="searchEvent" tabindex="-1" aria-labelledby="searchEventLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5 fw-bold" id="searchEventLabel">
+                    <i class="fa-solid fa-search me-2"></i> Search Event
+                </h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="" method="get">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="keyword" name="keyword" placeholder="Enter search keyword">
+                    </div>
+                    <div class="d-flex align-items-center justify-content-end gap-2 mt-3">
+                        <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-dark">Search</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
