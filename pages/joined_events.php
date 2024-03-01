2da272ea592ac2093d7177f0bfa74e9abbd013bd
Rafif Athallah <rafifathallah99@gmail.com>
2024-03-01 15:50:18 +0700

    update: add dropdown menu for details and unjoin event

--- pages/joined_events.php
+++ pages/joined_events.php
@@ -10,7 +10,8 @@ $id = $_SESSION["user_id"];
 $events = sqlQuery("SELECT events.* FROM participants
                     JOIN events ON participants.event_id = events.event_id
                     WHERE participants.user_id = '$id'
-                    ORDER BY CASE
+                    ORDER BY
+                        CASE
                             WHEN events.event_date >= CURRENT_DATE() THEN 0
                             ELSE 1
                         END,
@@ -25,7 +26,18 @@ if (isset($_GET["keyword"])) {
     }
 }
 
-if (isset($_POST["logout"])) {
+if (isset($_POST["unjoinEvent"])) {
+
+    if (unjoinEvent($_POST) > 0) {
+        echo "
+            <script>
+                alert('You have cancelled event participation!');
+                window.location.href = 'joined_events.php';
+            </script>";
+    }
+}
+
+else if (isset($_POST["logout"])) {
     logoutAccount();
     exit;
 }
@@ -70,7 +82,7 @@ if (isset($_POST["logout"])) {
                             </div>
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title fw-bold mb-2">
+                            <h5 class="card-title fw-bold mb-2 text-truncate">
                                 <?= $row['event_name']; ?>
                             </h5>
                             <p class="card-text text-secondary d-flex align-items-center">
@@ -81,7 +93,7 @@ if (isset($_POST["logout"])) {
                                 <?= $row['event_description']; ?>
                             </p>
                             <div class="row g-2">
-                                <div class="col-12 col-lg-10">        
+                                <div class="col-12 col-lg-8">        
                                     <?php 
                                     $isUpcoming = timeToEvent($row['event_date']);         
                                     $isPassed = isDatePassed($row['event_date']); 
@@ -100,23 +112,38 @@ if (isset($_POST["logout"])) {
                                         <?php endif; ?>
                                     </form>
                                 </div>
-                                <div class="col-12 col-lg-2">
-                                    <a href="event_details.php?id=<?= $row['event_id']; ?>"
-                                        class="btn btn-outline-secondary w-100">
-                                        <i class="fa-solid fa-info-circle"></i>
-                                        <span class="d-lg-none ms-2">Details</span>
-                                    </a>
+                                <div class="col-12 col-lg-4">
+                                    <div class="dropup">
+                                        <button class="btn btn-outline-secondary dropdown-toggle w-100" type="button"
+                                            id="eventMenu" data-bs-toggle="dropdown" aria-expanded="false">
+                                            More
+                                        </button>
+                                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="eventMenu">
+                                            <li>
+                                                <a class="dropdown-item" href="event_details.php?id=<?= $row['event_id']; ?>">Details</a>
+                                            </li>
+                                            <li><hr class="dropdown-divider"></li>
+                                            <li>
+                                                <button class="btn btn-link dropdown-item text-danger fw-bold" data-bs-toggle="modal" data-bs-target="#unjoinID<?= $row['event_id']; ?>" <?= ($isPassed) ? 'disabled' : '' ?>>
+                                                    <i class="fa-solid fa-calendar-minus me-2"></i>Unjoin
+                                                </button>
+                                            </li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+
+                <?php include "../includes/events.unjoin.php"; ?>
+
             <?php endforeach; ?>
         </div>
     </div>
 
     <?php include "../includes/events.search.php"; ?>
-
+    
     <?php include "../includes/logout.popup.php"; ?>
 </main>
 
